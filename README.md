473deb5f6d09b3f3ac4cfe08f3346bbb5406b632
Andrew Lin <andrewlin12@gmail.com>
2011-10-27 21:55:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+jcrop-canvas
+============
+Integrates the jQuery Jcrop plugin with an HTML5 canvas so you can do
+client-side cropping.
+
+Prerequisites
+=============
+jQuery
+Jcrop (https://github.com/tapmodo/Jcrop)
+
+Browser Support
+===============
+Chrome 8+
+FF 4+
+Safari nightly-builds (needs window.URL.createObjectURL)
+
+Options
+=======
+ - boxWidth: the width of the source image
+ - height: the height of the crop
+ - mimeType: 'image/jpg' or 'image/png'
+ - sourceImageSelector: the <img> which will house the image to be cropped
+ - previewContainerSelector: the element which will house the crop
+ - width: the width of the crop
+
+Example Usage
+=============
+See example.html 
+
+NOTE: Run it through a webserver, not as a local file (python -m 
+SimpleHTTPServer)
---
+++ jquery.jcrop-canvas.js
@@ -0,0 +1,173 @@
+(function($) {
+  window.URL = window.webkitURL || window.URL;
+
+  // From http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
+  function dataURItoBlob(dataURI, callback) {
+    var BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder;
+
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    // write the ArrayBuffer to a blob, and you're done
+    var bb = new BlobBuilder();
+    bb.append(ab);
+    return bb.getBlob(mimeString);
+  }
+
+  var methods = {
+    init: function(options) {
+      var self = this;
+      var cropper = {};
+
+      self.options = {
+        boxWidth: 600,
+        height: 100,
+        mimeType: 'image/png',
+        sourceImageSelector: 'img.source',
+        previewContainerSelector: 'div.preview-container',
+        width: 100,
+      };
+
+      var updatePreview = function(coords) {
+        self.currentCrop = coords;
+        var context = self.preview.get(0).getContext('2d');
+        if (self.currentCrop.w > 0 && self.currentCrop.h > 0) {
+          context.drawImage(self.source.get(0),
+            self.currentCrop.x, self.currentCrop.y,
+            self.currentCrop.w, self.currentCrop.h,
+            0, 0, self.options.width, self.options.height);
+        }
+      };
+
+      self.setupCropper = function() {
+        self.source.Jcrop({
+          onChange: updatePreview,
+          onSelect: updatePreview,
+          aspectRatio: self.options.width / self.options.height,
+          boxWidth: self.options.boxWidth
+        });
+      };
+
+      self.loadImage = function(file) {
+        if (!window.URL) {
+          self.trigger('error',
+            'Please use Chrome 10+, Safari 4+, or FF 4+ to use the image cropper.');
+          return;
+        }
+
+        var jcropHolder = $('.jcrop-holder');
+        if (jcropHolder.length > 0) {
+          // remove jcrop
+          $.Jcrop(self.source).destroy();
+          jcropHolder.remove();
+        }
+
+        self.source.unbind('load');
+        self.source.load(function(e) {
+          // Cleanup the saved data
+          window.URL.revokeObjectURL(self.source.src);
+
+          self.setupCropper();
+
+          self.source.show();
+          self.previewContainer.show();
+
+          self.trigger('load');
+        });
+
+        self.source.attr('src', window.URL.createObjectURL(file));
+      };
+
+      $.extend(self.options, options);
+
+      self.bind('dragover',function(e) {
+        e.preventDefault();
+        return true;
+      });
+
+      self.bind('drop', function(e) {
+        e.preventDefault();
+        self.loadImage(e.originalEvent.dataTransfer.files[0]);
+      });
+
+      self.source = $(self.options.sourceImageSelector);
+
+      self.previewContainer = $(self.options.previewContainerSelector);
+      self.previewContainer.html('<canvas class="preview"></canvas>');
+      self.preview = self.previewContainer.find('.preview');
+      self.preview.attr('width', self.options.width).attr('height', self.options.height);
+
+      self.find('input[type=file]').change(function() {
+        if (this.files.length > 0) {
+          self.loadImage(this.files[0]);
+        }
+      });
+
+      self.previewContainer.css(
+        'height', self.options.height
+      ).css(
+        'width', self.options.width
+      );
+
+      return this;
+    },
+    rotate: function() {
+      var self = this;
+      // Need to create a new properly-sized canvas, draw the image
+      // into the canvas then set the src attrs on both source and preview
+      // Then re-apply jcrop.  Fun.
+      var canvas = $('<canvas></canvas>');
+
+      // Create a canvas rotated by 90 degrees
+      var w = self.source.height();
+      var h = self.source.width();
+      canvas.attr('width', w);
+      canvas.attr('height', h);
+      canvas.hide();
+      self.previewContainer.after(canvas);
+
+      var context = canvas.get(0).getContext('2d');
+      context.translate(w, 0);
+      context.rotate(Math.PI / 2);
+      context.drawImage(self.source.get(0), 0, 0);
+
+      self.loadImage(dataURItoBlob(canvas.get(0).toDataURL('image/png')));
+
+      canvas.remove();
+    },
+    // Returns a data URI
+    createCrop: function() {
+      var self = this;
+      if (!self.currentCrop) {
+        return null;
+      }
+
+      var imgData = self.preview.get(0).toDataURL('image/png');
+
+      return imgData.split(";base64,")[1];
+    }
+  };
+
+  $.fn.jcropCanvas = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else if (typeof(method === 'object') || !method) {
+      return methods.init.apply(this, arguments);
+    }
+    else {
+      $.error('Method ' + method + ' does not exist on jQuery.jcropCanvas.');
+    }
+  };
+})(jQuery);
